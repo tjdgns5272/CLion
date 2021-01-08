cddba0f332bcaa8d026d32cbe3d8c2d305ecad53
tjdgns5272 <tjdgns5272@naver.com>
2021-01-08 12:07:02 +0900

    Initial Commit

---
+++ 42seoul/cmake-build-debug/CMakeFiles/42seoul.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "42seoul"
+  "42seoul.pdb"
+  "CMakeFiles/42seoul.dir/main.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/42seoul.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
